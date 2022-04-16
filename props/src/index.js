86bed5cef4ea6251e1c168e2ece5efb204569834
millerm30 <millergm30@gmail.com>
2022-04-16 11:34:15 -0230

    changes

--- props/src/index.js
+++ props/src/index.js
@@ -1,13 +1,15 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import Testing from './components/testing/Testing'
+import Think from './components/think/Think'
 
 const createEelement = document.querySelector('#root')
 const root = createRoot(createEelement)
 
 root.render(
     <StrictMode>
-        <App />
+        <Testing />
+        <Think />
     </StrictMode>
 )
