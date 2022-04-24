54824ccb8a2012fa3dc091292ad266d292271a8b
millerm30 <millergm30@gmail.com>
2022-04-24 19:04:53 -0230

    completed exercises

--- props/src/components/inputshow/Inputshow.js
+++ props/src/components/inputshow/Inputshow.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Inputshow = () => {
+  const [value, setValue] = useState('')
   return (
     <div className="container">
       <h2>Input Field Show Text On Type</h2>
       <p>User should be able to type in any characters on input and those characters should show in browser</p>
+      <input 
+      type='text'
+      placeholder='Enter text...'
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      />
+      <h4>{value}</h4>
     </div>
   );
 }
--- props/src/components/valuechange/Valuechange.js
+++ props/src/components/valuechange/Valuechange.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Valuechange = () => {
+  const [value, setValue] = useState('')
   return (
     <div className='container'>
         <h2>Button disabled until value on input</h2>
         <p>Make button disabled when there is no character on the input field. Enable the button when there is a atleast one character.</p>
+        <input 
+        type='text'
+        placeholder='Enter text...'
+        onChange={(e) => setValue(e.target.value)}
+        />
+        <button disabled={value.length < 1} style={{marginLeft: '1rem'}}>Submit</button>
     </div>
   )
 }
